a031f9053af5526faca967a58a36efd16cd9c334
hyun <zvg7777@gmail.com>
2021-03-17 18:26:08 +0900

    mongoDB -> mysql
    Used sequelize library

--- index.js
+++ index.js
@@ -1,34 +1,59 @@
 const express = require('express');
 const app = express();
-const mongoose = require('mongoose');
-//const bodyParser = require('body-parser'); // express 최신 버전에서는 더이상 필요 없음
+const models = require('./models/index')
+//const mysqldb = require('./config/database')();
+//const connection = mysqldb.init();
+
+//const mongoose = require('mongoose');
+const bodyParser = require('body-parser'); // express 최신 버전에서는 더이상 필요 없음
 const cookieParser = require('cookie-parser');
 // 환경별 config 값
-const config = require('./config/key');
+//const config = require('./config/key');
 
-const {User} = require('./models/user');
+//const {User} = require('./models/user');
 
-// db 연결
-mongoose.connect( config.mongoURI, {useNewUrlParser: true})
-    .then(() => console.log('DB Connected'))
-    .catch(err => console.error(err));
+//mysqldb.test_open(connection)
 
-// body , cookie 정보 받기
-//app.use(bodyParser.urlencoded({extended: true}))
-//app.use(bodyParser.json())
-app.use(cookieParser())
+/*models.sequelize.sync().then(() => {
+    console.log('DB Connected')
+}).catch(err => {
+    console.log('Error', err)
+})*/
 
-app.post('/api/users/register', (req, res) => {
-    const user = new User(req.body) // body parser 로 받은 값을 user 오브젝트 형태로 인스턴스화 해줌
-
-    //save = mongoose method (insert)
-    user.save((err, userData) => {
-        if (err) return res.json({success: false, err})
-        return res.status(200).json({success : true})
+app.get('/', (req, res) => {
+    models.users.findAll().then(result => {
+        res.json(result)
     })
-
 })
 
+
+// db 연결
+/*mongoose.connect( config.mongoURI, {useNewUrlParser: true})
+    .then(() => console.log('DB Connected'))
+    .catch(err => console.error(err));*/
+
+// body , cookie 정보 받기
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+app.use(cookieParser())
+
+
+app.post('/api/users/register', (req, res) => {
+
+    models.user.create({
+        email: req.body.email,
+        name: req.body.name,
+        lastname: req.body.lastname,
+        password: req.body.password
+    })
+        .then(result => {
+            console.log('data insert')
+        })
+        .catch(err => {
+            console.log('error', err)
+        })
+
+})
 
 
 app.listen(5000);
