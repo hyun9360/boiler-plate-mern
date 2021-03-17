6d82f4cc692535614cde1bba6fceaba3f8638d2b
hyun <zvg7777@gmail.com>
2021-03-17 18:31:35 +0900

    insert User

--- index.js
+++ index.js
@@ -1,18 +1,8 @@
 const express = require('express');
 const app = express();
 const models = require('./models/index')
-//const mysqldb = require('./config/database')();
-//const connection = mysqldb.init();
-
-//const mongoose = require('mongoose');
 const bodyParser = require('body-parser'); // express 최신 버전에서는 더이상 필요 없음
 const cookieParser = require('cookie-parser');
-// 환경별 config 값
-//const config = require('./config/key');
-
-//const {User} = require('./models/user');
-
-//mysqldb.test_open(connection)
 
 /*models.sequelize.sync().then(() => {
     console.log('DB Connected')
@@ -21,26 +11,19 @@ const cookieParser = require('cookie-parser');
 })*/
 
 app.get('/', (req, res) => {
-    models.users.findAll().then(result => {
+    models.User.findAll().then(result => {
         res.json(result)
     })
 })
-
-
-// db 연결
-/*mongoose.connect( config.mongoURI, {useNewUrlParser: true})
-    .then(() => console.log('DB Connected'))
-    .catch(err => console.error(err));*/
 
 // body , cookie 정보 받기
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 app.use(cookieParser())
 
-
 app.post('/api/users/register', (req, res) => {
 
-    models.user.create({
+    models.User.create({
         email: req.body.email,
         name: req.body.name,
         lastname: req.body.lastname,
