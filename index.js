cf80e4c9ad568892924bedbdb875b979eafb06dd
hyun <zvg7777@gmail.com>
2021-03-18 11:38:36 +0900

    password bcrypt remove mongoose email changeColumn -> unique true

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 const models = require('./models/index')
 const bodyParser = require('body-parser'); // express 최신 버전에서는 더이상 필요 없음
 const cookieParser = require('cookie-parser');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 /*models.sequelize.sync().then(() => {
     console.log('DB Connected')
@@ -23,18 +25,29 @@ app.use(cookieParser())
 
 app.post('/api/users/register', (req, res) => {
 
-    models.User.create({
-        email: req.body.email,
-        name: req.body.name,
-        lastname: req.body.lastname,
-        password: req.body.password
+    let body = req.body;
+
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+        if (err) return res.json({success: false, err});
+
+        bcrypt.hash(body.password, salt, function (err, hash) {
+            if (err) return res.json({success: false, err});
+
+            // 암호화 성공 시 insert
+            models.User.create({
+                email: body.email,
+                name: body.name,
+                lastname: body.lastname,
+                password: hash
+            })
+                .then(result => {
+                    res.status(200).json({success: true, userData: result})
+                })
+                .catch(err => {
+                    res.json({success: false, err})
+                })
+        })
     })
-        .then(result => {
-            console.log('data insert')
-        })
-        .catch(err => {
-            console.log('error', err)
-        })
 
 })
 
