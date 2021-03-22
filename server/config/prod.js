a0895818c2c4a5f2e30d684272fee560fb2aa5bd
hyun <zvg7777@gmail.com>
2021-03-22 13:03:38 +0900

    env config

--- server/config/prod.js
+++ server/config/prod.js
@@ -1,3 +1,8 @@
 module.exports = {
-    mongoURI: process.env.MONGO_URI
+    host: process.env.MYSQL_HOST,
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASS,
+    port: process.env.MYSQL_PORT,
+    database: process.env.MYSQL_DB,
+    dialect: 'mysql'
 }
