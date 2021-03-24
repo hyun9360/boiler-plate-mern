6670f94a0b1b8a61970826efccd83ab4fd8230df
hyun <zvg7777@gmail.com>
2021-03-24 15:06:40 +0900

    materialize && loginPage

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,14 +1,16 @@
 import {Route, Switch} from 'react-router-dom'
 import About from "./about";
+import Login from './registerLogin'
 
 function App() {
-  return (
-    <div>
-      <Switch>
-          <Route path='/about' component={About} />
-      </Switch>
-    </div>
-  );
+    return (
+        <div>
+            <Switch>
+                <Route path='/about' component={About}/>
+                <Route path='/login' component={Login}/>
+            </Switch>
+        </div>
+    );
 }
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -5,12 +5,27 @@ import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from "react-router-dom";
 
+import "materialize-css/dist/css/materialize.min.css";
+import "materialize-css/dist/js/materialize.min.js";
+
+import {Provider} from 'react-redux'
+import {createStore, applyMiddleware} from "redux";
+import promiseMiddleware from 'redux-promise'
+import ReduxThunk from 'redux-thunk'
+
+import Reducer from './reducers'
+
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore)
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root')
+    <Provider
+        store={createStoreWithMiddleware(Reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())}>
+
+        <BrowserRouter>
+            <App/>
+        </BrowserRouter>
+    </Provider>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
